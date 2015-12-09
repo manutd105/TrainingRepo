345e2a97fc09497b115892f246fbbbd61f36a15d
Rahul Kulkarni <Rahul Kulkarni>
2015-12-09 17:30:44 -0500

    Adding the Bigger Number Javascript file AGAIN

--- FirstSet/if-else/biggerNumber.js
+++ FirstSet/if-else/biggerNumber.js
@@ -1,11 +1,23 @@
-function biggerNumber(number1, number2) {
-    if (number1 > number2) {
-        return number1;
+
+function biggerNum(number1, number2) {
+	
+	    if (number1 > number2) {
+        var a = "The greater number of " + number1 + " and " + number2 + " is " + number1;
+        return a;
+        
+        
     } 
     else {
-        return number2;
-    }
+        var b = "The greater number of " + number1 + " and " + number2 + " is " + number2;
+        return b;
+        
+        
+            }
+  
 }
 
-console.log(biggerNumber(67, 68));
+console.log(biggerNum(67,68));
+console.log(biggerNum(97,80));
+
+
 
