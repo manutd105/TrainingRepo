4aed37709b9cad6b2f674c4b0f34f8cbe3f96d7e
Rahul Kulkarni <Rahul Kulkarni>
2015-12-09 20:19:49 -0500

    Shwoing full program for the user

--- FirstSet/if-else/biggerNumber.js
+++ FirstSet/if-else/biggerNumber.js
@@ -1,23 +1,25 @@
 
-function biggerNum(number1, number2) {
-	
-	    if (number1 > number2) {
-        var a = "The greater number of " + number1 + " and " + number2 + " is " + number1;
-        return a;
+function biggerNum() {
+	var number1 = document.getElementById("num1").value;
+	var number2 = document.getElementById("num2").value;
+	var element3 = document.getElementById("num3");
+
+	var message = "The Greater number of " + number1 + " and " + number2 + " is ";
+
+	console.log(number1, number2);
+	if (number1 > number2) {
+        message = message + number1;       
         
+    } else {
+        message = message + number2;             
         
-    } 
-    else {
-        var b = "The greater number of " + number1 + " and " + number2 + " is " + number2;
-        return b;
-        
-        
-            }
-  
+    }
+    console.log(message); 
+    element3.innerHTML = message;
 }
 
-console.log(biggerNum(67,68));
-console.log(biggerNum(97,80));
+//console.log(biggerNum(67,68));
+//console.log(biggerNum(97,80));
 
 
 
